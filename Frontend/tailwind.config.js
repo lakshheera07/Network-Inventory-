3ea65c6e3ae626cd1f404e4dd9057e25c0b4b1cb
lakshheer18 <Laksh.Heera@team.telstra.com>
2025-09-04 13:40:12 +0530

    Scanner gif

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+    pulseWave: {
+      "0%": { transform: "translate(-50%, -50%) scale(0.5)", opacity: "0.8" },
+      "100%": { transform: "translate(-50%, -50%) scale(1)", opacity: "0" },
+    },
+    connect: {
+      "0%": { opacity: "0", transform: "translate(-50%, -50%) scale(0.8)" },
+      "50%": { opacity: "1", transform: "translate(-50%, -50%) scale(1.1)" },
+      "100%": { opacity: "1", transform: "translate(-50%, -50%) scale(1)" },
+    },
+  },
+  animation: {
+    pulseWave: "pulseWave 2s infinite ease-out",
+    connect: "connect 2s ease-in-out forwards",
+  },
+    },
   },
   plugins: [],
 }
